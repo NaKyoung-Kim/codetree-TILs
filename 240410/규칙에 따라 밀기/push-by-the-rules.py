838d7d6e3d7b551169438ddc8d18c60dfb68c094
NaKyoung-Kim <78343969+NaKyoung-Kim@users.noreply.github.com>
2024-04-10 18:29:23 +0900

    Title: 규칙에 따라 밀기, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/규칙에 따라 밀기/push-by-the-rules.py
@@ -0,0 +1,19 @@
+str_in = input()
+command = input()
+
+LorR = 0
+
+for c in command:
+    if c == 'L':
+        LorR += 1
+    else:
+        LorR -= 1
+
+if LorR > 0:
+    move = str_in[:LorR]
+    str_in = str_in[LorR:]+move
+elif LorR < 0:
+    move = str_in[-(LorR):]
+    str_in = move+str_in[:LorR]
+
+print(str_in)
